6194c4780ce01cbbb5f45f174bc36466854a5c5f
clanec15 <forter125@outlook.com>
2020-10-07 22:03:58 -0500

    Update String_Hash_Generator_V1.2.py
    V1.3 added!
    
    SHA3 modes added and Error handling added

--- String_Hash_Generator_V1.2.py
+++ String_Hash_Generator_V1.2.py
@@ -1,8 +1,16 @@
-import hashlib
-print('          String Hash generator          ')
-from data import string
-import time
+#                                                                                                                                String Hash Generator V1.4                                                                                                      
 
+#all of the modules necesary for the program (Don`t touch this!)
+import time
+import hashlib
+#Warning that i dont know if it will be read or not
+print('¡WARNING! - if you have an outdated version of hashlib, blake modes will not work , so beware!')
+#This was before in data.py but i finally integrated both... yay
+input('Press enter to continue\n')
+print('          String Hash generator          ')
+string = input("Enter the string you want the hash generated\n")
+print("The string you entered was " +string)
+#all variables used in the first lines of code (I could have used a database or a funcion or another file but it seem easier this way
 a = '1'
 b = '2'
 c = '3'
@@ -11,20 +19,28 @@ e = '5'
 f = '6'
 g = '7'
 h = '8'
-print('___________________')
-print('|1 = MD5 mode     |')
-print('|2 = SHA256 mode  |')
-print('|3 = SHA1 mode    |')
-print('|4 = SHA224 mode  |')
-print('|5 = SHA384 mode  |')
-print('|6 = SHA512 mode  |')
-print('|7 = Blake2b mode |')
-print('|8 = Blake2s mode |')
-print('-------------------')
+i = '9'
+j = '10'
+k = '11'
+l = '12'
+#cute and relativly small menu for command prompt
+print('_________________________')
+print('|1  = MD5 mode          |')
+print('|2  = SHA256 mode       |')
+print('|3  = SHA1 mode         |')
+print('|4  = SHA224 mode       |')
+print('|5  = SHA384 mode       |')
+print('|6  = SHA512 mode       |')
+print('|7  = Blake2b mode      |')
+print('|8  = Blake2s mode      |')
+print('|9  = SHA3 224 mode     |')
+print('|10 = SHA3 256 mode     |')
+print('|11 = SHA3 384 mode     |')
+print('|12 = SHA3 512 mode     |')
+print('-------------------------')
 m = input('What mode do you want?\n')
 print('')
-
-
+#all modes in multiple elif statements (Its horrible i know but i lack caffein and donuts)
 if m == a:
     h = hashlib.md5()
 elif m == b:
@@ -41,24 +57,44 @@ elif m == g:
     h = hashlib.blake2b()
 elif m == h:
     h = hashlib.blake2s()
+elif m == i:
+    h = hashlib.sha3_224()
+elif m == j:
+    h = hashlib.sha3_256()
+elif m == k:
+    h = hashlib.sha3_384()
+elif m == l:
+    h = hashlib.sha3_512()
+#Both options of yes and no, i dont know how to make the variable recognice Y, yes, Yes or N and No
 x = 'y'
 y = 'n'
 
 h.update(string.encode('utf-8'))
 s = input('Want to save it to a file?\n')
+#saving the hash to a file, for the moment i cant make the program to change the name
 if s == x:
-    print('Saving...')
-    time.sleep(3)
-    f = open("Str_to_hash.txt", "x")
-    f = open("Str_to_hash.txt", "w")
-    f.write("This is the hash that was created... ")
-    f.write(h.hexdigest())
-    f.close()
-
+    try:
+        print('Saving...')
+        time.sleep(3)
+        f = open("Str_to_hash.txt", "x")
+        f = open("Str_to_hash.txt", "w")
+        f.write("This is the hash that was created... ")
+        f.write("")
+        f.write(h.hexdigest())
+        f.close()
+        print('file saved!')
+#Excepcion Handling, the worst way possible (i think)
+    except FileExistsError:
+        print('')
+        print('File already exists please delete or rename the previus text file')
+	
+               
 elif s == y:
     print('This is the hash that you were looking for...')
     print(h.hexdigest())
        
 print('')
+#Small nod to KMSpico
 print("Nothing more to do here, Quitting...")
-time.sleep(5)
+time.sleep(10)
+
